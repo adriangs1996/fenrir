tmacc/agentdynamo

chore(desktop): separate dev AppUserModelID on Windows (#1934)

apps/desktop/src/main.ts
@@ -100,7 +100,7 @@ const DESKTOP_SCHEME = "t3";
 const ROOT_DIR = Path.resolve(__dirname, "../../..");
 const isDevelopment = Boolean(process.env.VITE_DEV_SERVER_URL);
 const APP_DISPLAY_NAME = isDevelopment ? "T3 Code (Dev)" : "T3 Code (Alpha)";
-const APP_USER_MODEL_ID = "com.t3tools.t3code";
+const APP_USER_MODEL_ID = isDevelopment ? "com.t3tools.t3code.dev" : "com.t3tools.t3code";
 const LINUX_DESKTOP_ENTRY_NAME = isDevelopment ? "t3code-dev.desktop" : "t3code.desktop";
 const LINUX_WM_CLASS = isDevelopment ? "t3code-dev" : "t3code";
 const USER_DATA_DIR_NAME = isDevelopment ? "t3code-dev" : "t3code";

adriangs1996/fenrir

apps/desktop/src/main.ts
@@ -131,7 +131,7 @@ const DESKTOP_SCHEME = "t3";
 const ROOT_DIR = Path.resolve(__dirname, "../../..");
 const isDevelopment = Boolean(process.env.VITE_DEV_SERVER_URL);
 const APP_DISPLAY_NAME = isDevelopment ? "Fenrir (Dev)" : "Fenrir";
-const APP_USER_MODEL_ID = "com.fenrir.app";
+const APP_USER_MODEL_ID = isDevelopment ? "com.fenrir.app.dev" : "com.fenrir.app";
 const LINUX_DESKTOP_ENTRY_NAME = isDevelopment ? "fenrir-dev.desktop" : "fenrir.desktop";
 const LINUX_WM_CLASS = isDevelopment ? "fenrir-dev" : "fenrir";
 const USER_DATA_DIR_NAME = isDevelopment ? "fenrir-dev" : "fenrir";